504feaf21fe929a788b97e6367984a24eaf9095d
zhurgut <damian.ca@bluewin.ch>
2026-02-20 17:32:32 +0100

    some functions to check how far out we need to check for the closest points

--- src/Voronoi.jl
+++ src/Voronoi.jl
@@ -1,269 +1,390 @@
-# distance from border
+
+# distances from points 
 
 
-voronoi_noise(coords...) = voronoi_noise(Float64.(coords)...)
+let points::Matrix{Float64} = zeros(16, Threads.nthreads()),
+    index::Vector{pad(Int)} = zeros(pad(Int), Threads.nthreads())
+    nr_points::Vector{pad(Int)} = zeros(pad(Int), Threads.nthreads())
 
-function voronoi_noise(x)
-    # l2...(pl2)...l..x.(pl)....r....(pr)....r+1...
-    l::Int = floor(x)
-    r = l+1
-    l2 = l-1
-    pl2 = rand30_from(l2 ⊻ SEED)
-    pl = rand30_from(l ⊻ SEED)
-    pr = rand30_from(r ⊻ SEED)
-    dx = x - l
-    dl2 = dx + 1-pl2
-    dl = abs(pl - dx)
-    dr = pr + 1-dx
-    return min(dl2, dl, dr)
+    function get_points!(idx, pos)
+        ti = Threads.threadid()
+
+        r, h = random_from(pos)
+
+        p, h = more_random_from(h)
+        points[idx, ti] = pos + p
+        idx += 1
+        
+        if r < 0.1
+            p, h = more_random_from(h)
+            points[idx, ti] = pos + p
+            idx += 1
+        end
+
+        if r < 0.02
+            p, h = more_random_from(h)
+            points[idx, ti] = pos + p
+            idx += 1
+        end
+
+        return idx
+    end
+
+    function load()
+        ti = Threads.threadid()
+        @view points[1:nr_points[ti].value, ti]
+    end
+
+    function store!(l)
+        i = 1
+        for f=-2:2
+            i = get_points!(i, l+f)
+        end
+
+        ti = Threads.threadid()
+        len = i-1
+        nr_points[ti] = Padding(len, nr_points[ti].padding)
+        index[ti] = Padding(l, index[ti].padding)
+
+        @view points[1:len, ti]
+    end
+
+    store!(0)
+
+
+    global function voronoi_points(x)
+        l, r, d = bounds(x)
+        ti = Threads.threadid()
+        return index[ti].value == l ? load() : store!(l)
+    end
+
 end
 
 
-# 2D
-let coords::Vector{Int} = Int[typemin(Int), typemin(Int)],
-    points::Vector{Tuple{Float64, Float64}} = Vector{Tuple{Float64, Float64}}(undef, 25),
-    nr_inner_points::Int = 0,
-    got_outer::Bool = false
+let points::Matrix{NTuple{2, Float64}} = zeros(NTuple{2, Float64}, (64, Threads.nthreads())),
+    index::Vector{pad(Tuple{Int, Int})} = zeros(pad(Tuple{Int, Int}), Threads.nthreads())
+    nr_points::Vector{pad(Int)} = zeros(pad(Int), Threads.nthreads())
 
-    function push_points(x::Int, y::Int, sx::Int, sy::Int)
-        nr_points = (randn30_from(x+sx ⊻ SEED, y+sy ⊻ SEED) |> abs |> floor |> Int) + 1
-        for i=1:nr_points
-            px = more_rand30_from()
-            py = more_rand30_from()
-            push!(points, (sx+px, sy+py))
+    function get_points!(idx, p1, p2)
+        ti = Threads.threadid()
+
+        r, h = random_from(p1, p2)
+
+        x, h = more_random_from(h)
+        y, h = more_random_from(h)
+        points[idx, ti] = (p1+x, p2+y)
+        idx += 1
+        
+        if r < 0.1
+            x, h = more_random_from(h)
+            y, h = more_random_from(h)
+            points[idx, ti] = (p1+x, p2+y)
+            idx += 1
         end
+
+        if r < 0.02
+            x, h = more_random_from(h)
+            y, h = more_random_from(h)
+            points[idx, ti] = (p1+x, p2+y)
+            idx += 1
+        end
+
+        return idx
     end
 
-    # x and y are the coordinates of the top left corner of the center square
-
-    # the 9 squares around the center, commonly, the closest point is in here
-    function get_center_points(x::Int, y::Int)
-        for i=-1:1, j=-1:1
-            push_points(x, y, i, j)
-        end
+    function load()
+        ti = Threads.threadid()
+        @view points[1:nr_points[ti].value, ti]
     end
 
-    function get_outer_points(x::Int, y::Int)
-        for i in -1:1, j in (-2, 2)
-            push_points(x, y, i, j)
-            push_points(x, y, j, i)
+    function store!(l, b)
+        i = 1
+        for x=-2:2, y=-2:2
+            if (abs(x) + abs(y) <= 3)
+                i = get_points!(i, l+x, b+y)
+            end
         end
+
+        ti = Threads.threadid()
+        len = i-1
+        nr_points[ti] = Padding(len, nr_points[ti].padding)
+        index[ti] = Padding((l, b), index[ti].padding)
+
+        @view points[1:len, ti]
     end
 
-    dist(t1, t2) = begin d1 = t1[1]-t2[1]; d2 = t1[2]-t2[2]; sqrt(d1*d1 + d2*d2) end
-
-    global function voronoi_noise(x::Float64, y::Float64)
-        @inbounds begin
-            ix::Int = floor(x)
-            iy::Int = floor(y)
-            px = x-ix
-            py = y-iy
-            if !(coords[1] == ix && coords[2] == iy)
-                coords[1] = ix
-                coords[2] = iy
-                empty!(points)
-                get_center_points(ix, iy)
-                nr_inner_points = length(points)
-                got_outer = false
-            end
-
-            min_dist = 2.0
-            for i in 1:nr_inner_points
-                d = dist(points[i], (px, py))
-                if d < min_dist
-                    min_dist = d
-                end
-            end
-
-            if min_dist < 1
-                return (1/1.4142135623730951)min_dist
-            end
-
-            if !got_outer
-                get_outer_points(ix, iy)
-                got_outer = true
-            end
-
-            for i in (nr_inner_points+1):length(points)
-                d = dist(points[i], (px, py))
-                if d < min_dist
-                    min_dist = d
-                end
-            end
-
-            return (1/1.4142135623730951)min_dist
-        end
-    end
-end
+    store!(0, 0)
 
 
-# 3D
-let coords::Vector{Int} = Int[typemin(Int), typemin(Int), typemin(Int)],
-    points::Vector{Tuple{Float64, Float64, Float64}} = Vector{Tuple{Float64, Float64, Float64}}(undef, 125),
-    nr_inner_points::Int = 0,
-    got_outer::Bool = false
+    global function voronoi_points(x, y)
+        l, r, dx = bounds(x)
+        b, t, dy = bounds(y)
+        ti = Threads.threadid()
 
-    function push_points(x, y, z, sx, sy, sz)
-        nr_points = (randn30_from(x+sx ⊻ SEED, y+sy ⊻ SEED, z+sz ⊻ SEED) |> abs |> floor |> Int) + 1
-        for i=1:nr_points
-            px = more_rand30_from()
-            py = more_rand30_from()
-            pz = more_rand30_from()
-            push!(points, (sx+px, sy+py, sz+pz))
-        end
+        return index[ti].value == (l, b) ? load() : store!(l, b)
     end
 
-    # x and y are the coordinates of the top left corner of the center square
-
-    # the 9 squares around the center, commonly, the closest point is in here
-    function get_center_points(x, y, z)
-        for i=-1:1, j=-1:1, k=-1:1
-            push_points(x, y, z, i, j, k)
-        end
-    end
-
-    function get_outer_points(x, y, z)
-        for i in -1:1, j in -1:1, k in (-2, 2)
-            push_points(x, y, z, i, j, k)
-            push_points(x, y, z, i, k, j)
-            push_points(x, y, z, k, i, j)
-        end
-    end
-
-    dist(t1, t2) = begin d1 = t1[1]-t2[1]; d2 = t1[2]-t2[2]; d3 = t1[3]-t2[3]; sqrt(d1*d1 + d2*d2 + d3*d3) end
-
-    global function voronoi_noise(x::Float64, y::Float64, z::Float64)
-        @inbounds begin
-            ix::Int = floor(x)
-            iy::Int = floor(y)
-            iz::Int = floor(z)
-            px = x-ix
-            py = y-iy
-            pz = z-iz
-            if !(coords[1] == ix && coords[2] == iy && coords[3] == iz)
-                coords[1] = ix
-                coords[2] = iy
-                coords[3] = iz
-                empty!(points)
-                get_center_points(ix, iy, iz)
-                nr_inner_points = length(points)
-                got_outer = false
-            end
-
-            min_dist = 2.0
-            for i in 1:nr_inner_points
-                d = dist(points[i], (px, py, pz))
-                if d < min_dist
-                    min_dist = d
-                end
-            end
-
-            if min_dist < 1
-                return (1/1.7320508075688772)min_dist
-            end
-
-            if !got_outer
-                get_outer_points(ix, iy, iz)
-                got_outer = true
-            end
-
-            for i in (nr_inner_points+1):length(points)
-                d = dist(points[i], (px, py, pz))
-                if d < min_dist
-                    min_dist = d
-                end
-            end
-
-            return (1/1.7320508075688772)min_dist
-        end
-    end
 end
 
 
 
-# 4D
-let coords::Vector{Int} = Int[typemin(Int), typemin(Int), typemin(Int), typemin(Int)],
-    points::Vector{Tuple{Float64, Float64, Float64, Float64}} = Vector{Tuple{Float64, Float64, Float64, Float64}}(undef, 300),
-    nr_inner_points::Int = 0,
-    got_outer::Bool = false
+let points::Matrix{NTuple{3, Float64}} = zeros(NTuple{3, Float64}, (160, Threads.nthreads())),
+    index::Vector{pad(Tuple{Int, Int, Int})} = zeros(pad(Tuple{Int, Int, Int}), Threads.nthreads())
+    nr_points::Vector{pad(Int)} = zeros(pad(Int), Threads.nthreads())
 
-    function push_points(x, y, z, w, sx, sy, sz, sw)
-        nr_points = (randn30_from(x+sx ⊻ SEED, y+sy ⊻ SEED, z+sz ⊻ SEED, w+sw ⊻ SEED) |> abs |> floor |> Int) + 1
-        for i=1:nr_points
-            px = more_rand30_from()
-            py = more_rand30_from()
-            pz = more_rand30_from()
-            pw = more_rand30_from()
-            push!(points, (sx+px, sy+py, sz+pz, sw+pw))
+    function get_points!(idx, p1, p2, p3)
+        ti = Threads.threadid()
+
+        r, h = random_from(p1, p2, p3)
+
+        x, h = more_random_from(h)
+        y, h = more_random_from(h)
+        z, h = more_random_from(h)
+        points[idx, ti] = (p1+x, p2+y, p3+z)
+        idx += 1
+        
+        if r < 0.1
+            x, h = more_random_from(h)
+            y, h = more_random_from(h)
+            z, h = more_random_from(h)
+            points[idx, ti] = (p1+x, p2+y, p3+z)
+            idx += 1
         end
+
+        if r < 0.02
+            x, h = more_random_from(h)
+            y, h = more_random_from(h)
+            z, h = more_random_from(h)
+            points[idx, ti] = (p1+x, p2+y, p3+z)
+            idx += 1
+        end
+
+        return idx
     end
 
-    # x and y are the coordinates of the top left corner of the center square
-
-    # the 9 squares around the center, commonly, the closest point is in here
-    function get_center_points(x, y, z, w)
-        for i=-1:1, j=-1:1, k=-1:1, l=-1:1
-            push_points(x, y, z, w, i, j, k, l)
-        end
+    function load()
+        ti = Threads.threadid()
+        @view points[1:nr_points[ti].value, ti]
     end
 
-    function get_outer_points(x, y, z, w)
-        for i in -1:1, j in -1:1, l in -1:1, k in (-2, 2)
-            push_points(x, y, z, w, i, j, l, k)
-            push_points(x, y, z, w, i, j, k, l)
-            push_points(x, y, z, w, i, k, j, l)
-            push_points(x, y, z, w, k, i, j, l)
+    function store!(l, b, a)
+        i = 1
+        for x=-2:2, y=-2:2, z=-2:2
+            if (abs(x) + abs(y) + abs(z) <= 5)
+                i = get_points!(i, l+x, b+y, a+z)
+            end
         end
+
+        ti = Threads.threadid()
+        len = i-1
+        nr_points[ti] = Padding(len, nr_points[ti].padding)
+        index[ti] = Padding((l, b, a), index[ti].padding)
+
+        @view points[1:len, ti]
     end
 
-    dist(t1, t2) = begin d1 = t1[1]-t2[1]; d2 = t1[2]-t2[2]; d3 = t1[3]-t2[3]; d4 = t1[4]-t2[4]; sqrt(d1*d1 + d2*d2 + (d3*d3 + d4*d4)) end
+    store!(0, 0, 0)
 
-    global function voronoi_noise(x::Float64, y::Float64, z::Float64, w::Float64)
-        @inbounds begin
-            ix::Int = floor(x)
-            iy::Int = floor(y)
-            iz::Int = floor(z)
-            iw::Int = floor(w)
-            px = x-ix
-            py = y-iy
-            pz = z-iz
-            pw = w-iw
-            if !(coords[1] == ix && coords[2] == iy && coords[3] == iz && coords[4] == iw)
-                coords[1] = ix
-                coords[2] = iy
-                coords[3] = iz
-                coords[4] = iw
-                empty!(points)
-                get_center_points(ix, iy, iz, iw)
-                nr_inner_points = length(points)
-                got_outer = false
+    global function voronoi_points(x, y, z)
+        l, r, dx = bounds(x)
+        b, t, dy = bounds(y)
+        a, o, dz = bounds(z)
+        ti = Threads.threadid()
+
+        return index[ti].value == (l, b, a) ? load() : store!(l, b, a)
+    end
+
+end
+
+
+
+
+let points::Matrix{NTuple{4, Float64}} = zeros(NTuple{4, Float64}, (360, Threads.nthreads())),
+    index::Vector{pad(Tuple{Int, Int, Int, Int})} = zeros(pad(Tuple{Int, Int, Int, Int}), Threads.nthreads())
+    nr_points::Vector{pad(Int)} = zeros(pad(Int), Threads.nthreads())
+
+    function get_points!(idx, p1, p2, p3, p4)
+        ti = Threads.threadid()
+
+        r, h = random_from(p1, p2, p3, p4)
+
+        x, h = more_random_from(h)
+        y, h = more_random_from(h)
+        z, h = more_random_from(h)
+        w, h = more_random_from(h)
+        points[idx, ti] = (p1+x, p2+y, p3+z, p4+w)
+        idx += 1
+        
+        if r < 0.1
+            x, h = more_random_from(h)
+            y, h = more_random_from(h)
+            z, h = more_random_from(h)
+            w, h = more_random_from(h)
+            points[idx, ti] = (p1+x, p2+y, p3+z, p4+w)
+            idx += 1
+        end
+
+        if r < 0.02
+            x, h = more_random_from(h)
+            y, h = more_random_from(h)
+            z, h = more_random_from(h)
+            w, h = more_random_from(h)
+            points[idx, ti] = (p1+x, p2+y, p3+z, p4+w)
+            idx += 1
+        end
+
+        return idx
+    end
+
+    function load()
+        ti = Threads.threadid()
+        @view points[1:nr_points[ti].value, ti]
+    end
+
+    function store!(l, b, a, v)
+        i = 1
+        
+        for x=-2:2, y=-2:2, z=-2:2, w=-2:2
+            if (abs(x) + abs(y) + abs(z) + abs(w) <= 7)
+                i = get_points!(i, l+x, b+y, a+z, v+w)
+                # println(i)
             end
+        end
 
-            min_dist = 3.0
-            for i in 1:nr_inner_points
-                d = dist(points[i], (px, py, pz, pw))
-                if d < min_dist
-                    min_dist = d
-                end
-            end
+        ti = Threads.threadid()
+        len = i-1
+        nr_points[ti] = Padding(len, nr_points[ti].padding)
+        index[ti] = Padding((l, b, a, v), index[ti].padding)
 
-            if min_dist < 1
-                return 0.5min_dist
-            end
+        @view points[1:len, ti]
+    end
 
-            if !got_outer
-                get_outer_points(ix, iy, iz, iw)
-                got_outer = true
-            end
+    store!(0, 0, 0, 0)
 
-            for i in (nr_inner_points+1):length(points)
-                d = dist(points[i], (px, py, pz, pw))
-                if d < min_dist
-                    min_dist = d
-                end
-            end
 
-            return 0.5min_dist
+    global function voronoi_points(x, y, z, w)
+        l, r, dx = bounds(x)
+        b, t, dy = bounds(y)
+        a, o, dz = bounds(z)
+        v, _, _  = bounds(w)
+        ti = Threads.threadid()
+
+        return index[ti].value == (l, b, a, v) ? load() : store!(l, b, a, v)
+    end
+
+end
+
+
+
+
+
+function min_max_dists2d()
+    ds = []
+    relevant = []
+    for l=-3:3, b=-3:3
+        r = l+1
+        close_h, far_h = abs(l) < abs(r) ? (l, r) : (r, l)
+        t = b+1
+        close_v, far_v = abs(b) < abs(t) ? (b, t) : (t, b)
+        close_d = sqrt(close_h^2 + close_v^2)
+        far_d = sqrt(far_h^2 + far_v^2)
+        push!(ds, ((l, b), close_d, far_d))
+        if (close_d < sqrt(2))
+            push!(relevant, (b, l))
         end
     end
+    display(relevant)
+    by_l1 = []
+    for l1 = 0:5
+        mn = 10000
+        mx = 0
+        for d in ds
+            (l,b), cl, fr = d
+            l1_dist = abs(l) + abs(b)
+            if l1_dist == l1
+                mn = min(mn, cl)
+                mx = max(mx, fr)
+            end
+        end
+        push!(by_l1, (l1, mn, mx))
+    end
+    display(by_l1)
+end
+
+function min_max_dists3d()
+    ds = []
+    relevant = []
+    for l=-3:3, b=-3:3, a=-3:3
+        r = l+1
+        close_h, far_h = abs(l) < abs(r) ? (l, r) : (r, l)
+        t = b+1
+        close_v, far_v = abs(b) < abs(t) ? (b, t) : (t, b)
+        o = a+1
+        close_q, far_q = abs(a) < abs(o) ? (a, o) : (o, a)
+        close_d = sqrt(close_h^2 + close_v^2 + close_q^2)
+        far_d = sqrt(far_h^2 + far_v^2 + far_q^2)
+        push!(ds, ((l, b, a), close_d, far_d))
+        if (close_d < sqrt(3))
+            push!(relevant, (b, l, a))
+        end
+    end
+    for r in relevant
+        println(r)
+    end
+
+    by_l1 = []
+    for l1 = 0:9
+        mn = 10000
+        mx = 0
+        for d in ds
+            (l,b,a), cl, fr = d
+            l1_dist = abs(l) + abs(b) + abs(a)
+            if l1_dist == l1
+                mn = min(mn, cl)
+                mx = max(mx, fr)
+            end
+        end
+        push!(by_l1, (l1, mn, mx))
+    end
+    display(by_l1)
+end
+
+function min_max_dists4d()
+    ds = []
+    relevant = []
+    for l=-3:3, b=-3:3, a=-3:3, w=-3:3
+        r = l+1
+        close_h, far_h = abs(l) < abs(r) ? (l, r) : (r, l)
+        t = b+1
+        close_v, far_v = abs(b) < abs(t) ? (b, t) : (t, b)
+        o = a+1
+        close_q, far_q = abs(a) < abs(o) ? (a, o) : (o, a)
+        w2 = w+1
+        close_w, far_w = abs(w) < abs(w2) ? (w, w2) : (w2, w)
+        close_d = sqrt(close_h^2 + close_v^2 + close_q^2 + close_w^2)
+        far_d = sqrt(far_h^2 + far_v^2 + far_q^2 + far_w^2)
+        push!(ds, ((l, b, a, w), close_d, far_d))
+        if (close_d < sqrt(4))
+            push!(relevant, (b, l, a, w))
+        end
+    end
+    for r in relevant
+        println(r)
+    end
+
+    by_l1 = []
+    for l1 = 0:9
+        mn = 10000
+        mx = 0
+        for d in ds
+            (l,b,a, w), cl, fr = d
+            l1_dist = abs(l) + abs(b) + abs(a) + abs(w)
+            if l1_dist == l1 
+                mn = min(mn, cl)
+                mx = max(mx, fr)
+            end
+        end
+        push!(by_l1, (l1, mn, mx))
+    end
+    by_l1
 end
